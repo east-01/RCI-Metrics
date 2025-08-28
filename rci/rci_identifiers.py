1435ebc613d9ea6e7c75e56d26ad1842079e925c
Ethan Mullen <mullen.ethan@yahoo.com>
2025-08-28 13:42:43 -0700

    PromQL ingest plugin works, time to move to analyses

--- rci/rci_identifiers.py
+++ rci/rci_identifiers.py
@@ -1,42 +1,31 @@
 from dataclasses import dataclass
 
 from data.identifier import TimeStampIdentifier
+from src.utils.timeutils import get_range_printable
 
 @dataclass(frozen=True)
 class GrafanaIdentifier(TimeStampIdentifier):
     type: str
-    ingest_src: str # Talks abt either grafana, ingest_filesystem
-    ingest_cfg: str # Talks abt the configuration of the ingest controller
-
-@dataclass(frozen=True)
-class SourceIdentifier(TimeStampIdentifier):
-    """
-    Identifier for a source Grafana DataFrame.
-    """
-    type: str # cpu/gpu
+    query_cfg: str # Talks abt the configuration of the ingest controller, like monthly or csu
 
     def __hash__(self) -> int:
-        return hash((super().__hash__(), self.type))
+        return hash((super().__hash__(), self.type, self.query_cfg))
 
     def __eq__(self, other) -> bool:
-        return isinstance(other, SourceIdentifier) and super().__eq__(other) and self.type == other.type
+        return isinstance(other, GrafanaIdentifier) and super().__eq__(other) and self.type == other.type and self.query_cfg == other.query_cfg
 
     def __str__(self) -> str:
-        return f"sourcedata {self.type}:{self.start_ts}-{self.end_ts}"
-    
+        return f"grafana {self.query_cfg}, {self.type}, {get_range_printable(self.start_ts, self.end_ts, 3600)}"
+
 @dataclass(frozen=True)
-class SourceQueryIdentifier(SourceIdentifier):
-    """
-    Identifier for a source Grafana DataFrame. With additional information about the query that
-      generated it. Used in PromQL ingest process.
-    """
-    query_name: str # status/truth (-> values)
+class GrafanaIntermediateIdentifier(GrafanaIdentifier):
+    query_type: str # Talks abt the type of configuration, like status or values
 
     def __hash__(self) -> int:
-        return hash((super().__hash__(), self.query_name))
+        return hash((super().__hash__(), self.query_type))
 
     def __eq__(self, other) -> bool:
-        return isinstance(other, SourceQueryIdentifier) and super().__eq__(other) and self.query_name == other.query_name
+        return isinstance(other, GrafanaIntermediateIdentifier) and super().__eq__(other) and self.query_type == other.query_type
 
     def __str__(self) -> str:
-        return f"sourcequerydata {self.type} from query {self.query_name}:{self.start_ts}-{self.end_ts}"
+        return f"intermediate grafana {self.query_cfg}, {self.query_type}, {self.type}, {get_range_printable(self.start_ts, self.end_ts, 3600)}"
